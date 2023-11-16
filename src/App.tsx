df3a93549532560503ef191b14a4e489d493bf80
sailesh <sailesh@jobins.jp>
2023-11-16 18:17:03 +0545

    feat: pass props as string and object using typescript type

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./css/global.css";
 import Greet from "./components/Greet";
+import Person from "./components/Person";
 
 function App() {
+  const person = {
+    firstName: "Jhone",
+    lastName: "Doe",
+  };
   return (
     <div>
       <Greet name="sailesh" />
+      <Person name={person} />
     </div>
   );
 }
---
+++ src/components/Person.tsx
@@ -0,0 +1,18 @@
+type PersonProps = {
+  name: {
+    firstName: string;
+    lastName: string;
+  };
+};
+
+const Person = (props: PersonProps) => {
+  return (
+    <div>
+      Person: &nbsp;
+      {props.name.firstName} {props.name.lastName}
+    </div>
+  );
+};
+
+export default Person;
+
